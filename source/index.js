56ce33817b687d25ccbe473deba57169703d5a4c
Kien.NK <kien.nk@teko.vn>
2018-12-12 08:44:27 +0700

    reverse source index

--- source/index.js
+++ source/index.js
@@ -1,31 +1,31 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
-// const bodyParser = require('body-parser');
-// const fs = require('fs');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
 let app = express();
 let server = http.createServer(app);
 
 app.use('/', express.static(path.join(__dirname, 'build')));
-// app.use(bodyParser.urlencoded({extended: false}));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 const url = './source/public/template.json';
 
-// app.get('/template', function(request, response) {
-//   const jsonText = fs.readFileSync(url);
-//   const template = JSON.parse(jsonText);
-//   response.json(template);
-// });
+app.get('/template', function(request, response) {
+  const jsonText = fs.readFileSync(url);
+  const template = JSON.parse(jsonText);
+  response.json(template);
+});
 
-// app.post('/template', function(request, response) {
-//   const template = request.body;
-//   const jsonText = JSON.stringify(template);
-//   fs.writeFile(url, jsonText, (error) => {
-//     if (!error) response.end();
-//   });
-// });
+app.post('/template', function(request, response) {
+  const template = request.body;
+  const jsonText = JSON.stringify(template);
+  fs.writeFile(url, jsonText, (error) => {
+    if (!error) response.end();
+  });
+});
 
 const port = process.env.PORT || 3003;
 server.listen(port, function() {
