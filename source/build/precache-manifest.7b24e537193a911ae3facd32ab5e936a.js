401d7dee6277a2f0568a1a5b2fcd5366626e7657
WindzLord <kk.outplayed@gmail.com>
2018-12-12 21:47:02 +0700

    add .env client side

---
+++ source/build/precache-manifest.7b24e537193a911ae3facd32ab5e936a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0f9b1175e078e1f66406",
+    "url": "/static/js/main.0f9b1175.chunk.js"
+  },
+  {
+    "revision": "6ad6e5c719518dfedae4",
+    "url": "/static/js/1.6ad6e5c7.chunk.js"
+  },
+  {
+    "revision": "0f9b1175e078e1f66406",
+    "url": "/static/css/main.463e4ce9.chunk.css"
+  },
+  {
+    "revision": "6ad6e5c719518dfedae4",
+    "url": "/static/css/1.7c7152c4.chunk.css"
+  },
+  {
+    "revision": "1d8843f4245d58d290adefd41289269c",
+    "url": "/index.html"
+  }
+];
