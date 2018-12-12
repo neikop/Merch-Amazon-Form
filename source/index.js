875fbabc65ede909e29a6bc62970bd63ae06cbd9
Kien.NK <kien.nk@teko.vn>
2018-12-12 08:05:24 +0700

    reduce server index

--- source/index.js
+++ source/index.js
@@ -4,8 +4,8 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 
-const app = express();
-const server = http.createServer(app);
+let app = express();
+let server = http.createServer(app);
 
 app.use('/', express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.urlencoded({extended: false}));
@@ -13,19 +13,19 @@ app.use(bodyParser.json());
 
 const url = './source/public/template.json';
 
-app.get('/template', function(request, response) {
-  const jsonText = fs.readFileSync(url);
-  const template = JSON.parse(jsonText);
-  response.json(template);
-});
+// app.get('/template', function(request, response) {
+//   const jsonText = fs.readFileSync(url);
+//   const template = JSON.parse(jsonText);
+//   response.json(template);
+// });
 
-app.post('/template', function(request, response) {
-  const template = request.body;
-  const jsonText = JSON.stringify(template);
-  fs.writeFile(url, jsonText, (error) => {
-    if (!error) response.end();
-  });
-});
+// app.post('/template', function(request, response) {
+//   const template = request.body;
+//   const jsonText = JSON.stringify(template);
+//   fs.writeFile(url, jsonText, (error) => {
+//     if (!error) response.end();
+//   });
+// });
 
 const port = process.env.PORT || 3003;
 server.listen(port, function() {
